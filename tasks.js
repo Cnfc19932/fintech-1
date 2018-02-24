5d5371e058375ef7374e109e84a6b8a86aeaf318
Stanislav Gorovoy <cnfc19932@gmail.com>
2018-02-24 12:55:30 +0300

    problem 2

--- tasks.js
+++ tasks.js
@@ -16,7 +16,9 @@ function getMinMax(string) {
  * @return {number} число под номером х
  */
 function fibonacciSimple(x) {
-  return x;
+  if (x === 1) return 1;
+  if (x === 0) return 0;
+  return fibonacciSimple(x-2) + fibonacciSimple(x-1);
 }
 
 /* ============================================= */
